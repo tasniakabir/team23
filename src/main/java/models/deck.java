9623a995fa4554d11cbb5226e711465197b97ffa
padalah <padalah@oregonstate.edu>
2017-11-08 12:57:59 -0800

    Added a deck class - forgot to name it with a capital D; pleaze ignore naming convention issue.

---
+++ src/main/java/models/deck.java
@@ -0,0 +1,39 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class deck {
+
+    public java.util.List<Card> deck = new ArrayList<>();
+
+    public deck(){}
+
+    public void buildDeck() {
+        for(int i = 2; i < 15; i++){
+            deck.add(new Card(i,Suit.Clubs));
+            deck.add(new Card(i,Suit.Hearts));
+            deck.add(new Card(i,Suit.Diamonds));
+            deck.add(new Card(i,Suit.Spades));
+        }
+    }
+
+    public void shuffle() {
+        // shuffles the deck so that it is random
+        Random generator = new Random();
+        for(int i = 0; i < 52; i++){
+            //mod by 52 so that we don't leave scope of ArrayList
+            int random = Math.abs(generator.nextInt() % 52);
+            //swap locations
+            Card temp = deck.get(i);
+            deck.set(i, deck.get(random));
+            deck.set(random, temp);
+        }
+    }
+
+    public void decreaseDeck() {
+        for(int i = 0; i < 4; i++) {
+            deck.remove(deck.size() - 1);
+        }
+    }
+}
