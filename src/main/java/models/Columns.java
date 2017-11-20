508e4ad08cc75ccd7307831ac17b16835102fec8
dhanensm <dhanensm@oregonstate.edu>
2017-11-20 11:28:41 -0800

    fix column bug

--- src/main/java/models/Columns.java
+++ src/main/java/models/Columns.java
@@ -2,28 +2,15 @@ package models;
 
 import java.util.ArrayList;
 
-/**
- * Created by tasniakabir on 11/8/17.
- */
-public class Columns {
-    public java.util.List<java.util.List<Card>> cols = new ArrayList<>(4);
+public class Column {
 
-    private boolean columnHasCards(int columnNumber) {
-        // check indicated column for number of cards; if no cards return false, otherwise return true
-        if(cols.get(columnNumber).size()>0) {
-            return true;
-        }
-        return false;
+    public int id;
+    public java.util.List<Card> cards = new ArrayList<>();
+
+    public Column() {
     }
 
-    private Card getTopCard(int columnNumber) {
-        return this.cols.get(columnNumber).get(this.cols.get(columnNumber).size()-1);
-    }
-    private void addCardToCol(int columnTo, Card cardToMove) {
-        cols.get(columnTo).add(cardToMove);
-    }
-
-    private void removeCardFromCol(int colFrom) {
-        this.cols.get(colFrom).remove(this.cols.get(colFrom).size()-1);
+    public Column(int id) {
+        this.id = id;
     }
 }
