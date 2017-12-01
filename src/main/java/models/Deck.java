04a46a4f5c355775197783340ffa8d25684588d5
padalah <padalah@oregonstate.edu>
2017-11-30 19:57:35 -0800

    Erin and I refacotred Deck to follow OO principles

--- src/main/java/models/Deck.java
+++ src/main/java/models/Deck.java
@@ -1,49 +1,24 @@
 package models;
 
-import java.util.ArrayList;
-import java.util.Random;
+public class Deck extends superDeck {
 
-public class Deck {
-
-    public java.util.List<Card> deck = new ArrayList<>();
-
-    public Deck(){
+    public Deck() {
         buildDeck();
         shuffle();
     }
 
-    public Deck(int test){
-        if (test==1) {
+    public Deck(int test) {
+        if (test == 1) {
             buildDeck();
         }
     }
 
     private void buildDeck() {
-        for(int i = 2; i < 15; i++){
-            deck.add(new Card(i,Suit.Clubs));
-            deck.add(new Card(i,Suit.Hearts));
-            deck.add(new Card(i,Suit.Diamonds));
-            deck.add(new Card(i,Suit.Spades));
+        for (int i = 2; i < 15; i++) {
+            deck.add(new Card(i, Suit.Clubs));
+            deck.add(new Card(i, Suit.Hearts));
+            deck.add(new Card(i, Suit.Diamonds));
+            deck.add(new Card(i, Suit.Spades));
         }
-    }
-
-    private void shuffle() {
-        // shuffles the Deck so that it is random
-        Random generator = new Random();
-        for(int i = 0; i < 52; i++){
-            //mod by 52 so that we don't leave scope of ArrayList
-            int random = Math.abs(generator.nextInt() % 52);
-            //swap locations
-            Card temp = deck.get(i);
-            deck.set(i, deck.get(random));
-            deck.set(random, temp);
-        }
-    }
-
-    //this function returns the top card form the deck and removes said card from deck
-    public Card dealCard(){
-        Card tmp= deck.get(deck.size()-1);
-        deck.remove(deck.size() - 1);
-        return tmp;
     }
 }
